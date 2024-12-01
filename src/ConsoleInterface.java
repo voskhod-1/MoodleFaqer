0f818152d4bc2d4cda556d6f69c9e6d3c8078e6a
Anthony <avamkakoedelovich@ya.ru>
2024-12-02 01:32:04 +0300

    Initial commit

---
+++ src/ConsoleInterface.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+
+public class HelpFuncs {
+
+    public static void clearConsole(){
+        
+    }
+
+    public static void getMenu(){
+        System.out.println("Меню скрипта: \n1. Автоматическая посещаемость\n2. Нафармить клонов в лекцию\n3. Положить сайт\n4. Разлогиниться");
+    }
+}
---
+++ src/NowDateTime.java
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
+
+public class TimeSched {
+    static LocalDateTime today = LocalDateTime.now();
+
+    public static boolean isNumerator(boolean invert) {
+        int invertInt = 0;
+        if (invert) invertInt = 1;
+        WeekFields weekFields = WeekFields.of(Locale.ROOT);
+        int weekNumber = today.get(weekFields.weekOfYear());
+        return (weekNumber + invertInt) % 2 == 0;
+    }
+
+    public static String getDayOfWeek() {
+        return today.getDayOfWeek().toString();
+    }
+
+    public static String getTime(){
+        return today.toLocalTime().toString().substring(0, 5);
+    }
+}
